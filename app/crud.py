5aeb405c45ea3d12779cbb51cc847a38b69317ea
ntungufhadzeni <ntungufhadzenimbudzeni@gmail.com>
2023-06-17 22:35:26 +0200

    fix default bus error

--- app/crud.py
+++ app/crud.py
@@ -7,11 +7,11 @@ from sqlalchemy import and_, func
 def get_all_runs(db: Session):
     now = datetime.now()
     today = datetime.now().date()
-    return db.query(Run).filter(and_(func.date(Run.scheduled_end_time) == today, Run.scheduled_end_time > now, Run.vehicle_id != 38)).all()
+    return db.query(Run).filter(and_(func.date(Run.scheduled_end_time) == today, Run.scheduled_end_time > now, Run.vehicle_id <= 21)).all()
 
 def get_runs_by_run_name(db: Session, run_name: str):
     today = datetime.now().date()
-    return db.query(Run).filter(and_(func.date(Run.scheduled_end_time) == today, Run.vehicle_id != 38, Run.name.startswith(run_name))).all()
+    return db.query(Run).filter(and_(func.date(Run.scheduled_end_time) == today, Run.vehicle_id <= 21, Run.name.startswith(run_name))).all()
 
 
 def get_run_by_id(db: Session, run_id: int):
