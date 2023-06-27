b26a05c5fba3769bef2f4d455d955120af4690da
ntungufhadzeni <ntungufhadzenimbudzeni@gmail.com>
2023-06-27 08:34:40 +0200

    optimize engine

--- api/database.py
+++ api/database.py
@@ -6,7 +6,14 @@ from environs import Env
 env = Env()
 env.read_env()
 
-engine = create_engine(env.str('DB_STRING'))
+engine = create_engine(
+    env.str('DB_STRING'),
+    pool_size=100,
+    max_overflow=10,
+    pool_recycle=3600,
+    pool_timeout=30,
+    connect_args={'connect_timeout': 60},
+    isolation_level='READ UNCOMMITTED')
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
